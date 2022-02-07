630ed8f9fbaa1a0acdc37aea57a29c9a42ca4f96
Peter Pak <ppak10@gmail.com>
2022-02-06 16:22:06 -0800

    Adds examples for conversion.

---
+++ src/6_conversion/6_1_0_from.rs
@@ -0,0 +1,28 @@
+/*
+ * For example we can easily convert a `str` into a `String`
+ *
+ * ```
+ * let my_str = "hello";
+ * let my_string = String::from(my_str);
+ * ```
+ */
+
+// We can do similar for defining a conversion for our own type.
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+  value: i32,
+}
+
+impl From<i32> for Number {
+  fn from(item: i32) -> Self {
+    Number { value: item }
+  }
+}
+
+fn main() {
+  let num = Number::from(30);
+  println!("My number is {:?}", num);
+}
