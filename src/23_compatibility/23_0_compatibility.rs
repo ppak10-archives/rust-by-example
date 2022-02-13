ffbdf8d861a19c6af5a63063b3f3cb9e7570c2a2
Peter Pak <ppak10@gmail.com>
2022-02-12 16:04:17 -0800

    Adds examples for compatibility.

---
+++ src/23_compatibility/23_0_compatibility.rs
@@ -0,0 +1,9 @@
+/*
+ * Compatibility
+ *
+ * The Rust language is fastly evolving, and because of this certain
+ * compatibility issues can arise, despite efforts to ensure
+ * forwards-compatibility wherever possible.
+ *
+ * * Raw identifiers
+ */
